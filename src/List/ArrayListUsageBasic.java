8e02a64e6558549db47329b573ac1ce3c33a774a
Lenghong-dev <lenghongtiv@gmail.com>
2025-09-15 14:14:11 +0700

    second

--- src/List/ArrayListUsageBasic.java
+++ src/List/ArrayListUsageBasic.java
@@ -1,4 +1,28 @@
 package List;
 
-public class ArrayListUsageBasic {
+import java.util.ArrayList;
+
+class LinkedListUasgeBasic {
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<>();
+
+        names.add("Alice");
+        names.add("Bob");
+        names.add("Lita");
+        names.addFirst("Sinat");
+
+        System.out.println("First element: " + names.get(0));
+
+        names.set(1, "Devid");
+
+        names.remove(0);
+        names.remove("Bob");
+
+
+        for (String name : names) {
+            System.out.println(name);
+        }
+
+        System.out.println("Size of Array: " +names.size());
+    }
 }
--- src/List/ArrayListUsageObject.java
+++ src/List/ArrayListUsageObject.java
@@ -1,4 +1,29 @@
 package List;
 
+import java.util.ArrayList;
+
+class Student {
+    String name;
+    int age;
+
+    Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String toString() {
+        return name + " (" + age + ")";
+    }
+}
+
 public class ArrayListUsageObject {
+    public static void main(String[] args) {
+        ArrayList<Student> students = new ArrayList<>();
+        students.add(new Student("Alice", 20));
+        students.add(new Student("Bob", 22));
+
+        for (Student s : students) {
+            System.out.println(s);
+        }
+    }
 }
